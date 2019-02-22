13c66001ac66bc600a8171acea680cdb0aba1000
Rebecca Peltz <rebeccapeltz@gmail.com>
2019-02-22 15:23:45 -0800

    add api

--- deploy.py
+++ deploy.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return '<h1>Deployed to Heroku!</h1>'
 
+
+@app.route('/api')
+def api():
+    quiz = [{"type": "fillin", "question": "______ was the 44th president of the United States.",
+             "answer": "Barak Obama"},
+             {"type": "multichoice", "question": "The 44th President of the United States", 
+             "options":["Bill Clinton", "Jimmy Carter", "George Bush", "Barak Obama"],
+             "answer": "Barak Obama"}]
+    return jsonify(quiz)
